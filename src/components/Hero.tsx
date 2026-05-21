48cfb0f22ceb5f69d98b8d73518d483c8212da9b
essaghyrsiham-svg <essaghyrsiham@gmail.com>
2026-05-21 01:34:55 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
       </div>
 
       <div className="hero-image">
-        <img src="/hero-image.png" />
+        <img src="/IMG-20260521-WA0004.jpg" />
         
         <div className="stat-card card-1">
           <span>+127%</span>
